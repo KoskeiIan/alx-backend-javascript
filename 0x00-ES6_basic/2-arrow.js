55b8bdd28bd3d937ab72138f526ac46eddcd6e9c
KoskeiIan <ian.k.koskei@gmail.com>
2022-06-30 07:08:29 +0000

    00

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,4 +1,4 @@
-iexport default function getNeighborhoodsList() {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
-export default function returHowManyArguments(...args) {
+export default function returnHowManyArguments(...args) {
 	return args.length;
 }
