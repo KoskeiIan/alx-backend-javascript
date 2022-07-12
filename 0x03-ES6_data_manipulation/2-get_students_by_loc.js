7074f055d92fe13d51e2968f20aeb19290747486
KoskeiIan <ian.k.koskei@gmail.com>
2022-07-11 23:45:24 -0700

    2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,5 @@
-export default function getStudentsByLocation(ls, city) {
-	  return ls.filter((ls) => ls.location === city);
+export default function getStudentsByLocation(objArray, city) {
+	  if (!Array.isArray(objArray)) return [];
+
+	  return objArray.filter((obj) => obj.location === city);
 }
